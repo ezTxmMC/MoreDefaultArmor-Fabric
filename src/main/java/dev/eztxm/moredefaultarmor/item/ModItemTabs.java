92e5bca2c50ae8585d0686ff60e2f48acae97de8
ezTxmMC <eztxmmc@hotmail.com>
2024-07-29 13:12:23 +0200

    removed unused import

--- src/main/java/dev/eztxm/moredefaultarmor/item/ModItemTabs.java
+++ src/main/java/dev/eztxm/moredefaultarmor/item/ModItemTabs.java
@@ -4,7 +4,6 @@ import dev.eztxm.moredefaultarmor.MoreDefaultArmor;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
