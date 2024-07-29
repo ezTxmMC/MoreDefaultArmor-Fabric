8c05ebca7fe93adf6c207be0e59e37d32fd56b3f
ezTxmMC <eztxmmc@hotmail.com>
2024-07-29 16:14:53 +0200

    added UpdateChecker

--- src/main/java/dev/eztxm/moredefaultarmor/MoreDefaultArmor.java
+++ src/main/java/dev/eztxm/moredefaultarmor/MoreDefaultArmor.java
@@ -2,15 +2,37 @@ package dev.eztxm.moredefaultarmor;
 
 import dev.eztxm.moredefaultarmor.item.ModItemTabs;
 import dev.eztxm.moredefaultarmor.item.ModItems;
+import dev.eztxm.moredefaultarmor.util.UpdateChecker;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.Optional;
 
 public class MoreDefaultArmor implements ModInitializer {
     public static final String MOD_ID = "moredefaultarmor";
+
+    private static UpdateChecker updateChecker;
 
     @Override
     public void onInitialize() {
         ModItemTabs.registerItemGroups();
         ModItems.registerItems();
         ModItemTabs.setupItemGroups();
+
+        Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(MOD_ID);
+        modContainer.ifPresent(container -> updateChecker = new UpdateChecker(container.getMetadata().getVersion().getFriendlyString()));
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            server.getPlayerManager().getPlayerList().forEach(this::onPlayerJoin);
+        });
+    }
+
+    private void onPlayerJoin(ServerPlayerEntity player) {
+        if (!updateChecker.latestVersion()) {
+            player.sendMessage(Text.translatable("message.moredefaultarmor.update"));
+        }
     }
 }
